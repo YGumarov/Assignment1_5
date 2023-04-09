a2156e6153202e878cbb3e90b2b24ea6407ff49d
YGumarov <yerassylgumarov@gmail.com>
2023-04-09 13:47:19 +0600

    Assignment1_5

---
+++ Assignment1_5/Program.cs
@@ -0,0 +1,19 @@
+﻿static int Fibonacci(int n)
+{
+    int result;
+    if (n == 1)
+    {
+        return 1;
+    }
+    if (n == 0)
+    {
+        return 0;
+    }
+    
+    result = Fibonacci(n - 1) + Fibonacci(n - 2);
+    return result;
+    
+}
+
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine(Fibonacci(n));
